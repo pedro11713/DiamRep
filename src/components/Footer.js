471cc70e32b4c9ee77f495fa39602075dc154be3
Pedro <pedro.reis_sena@smail.th-koeln.de>
2025-03-23 19:30:11 +0000

    Exercicio 5.6 feito e inicio do 5.7 no FormVoluntario.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,20 @@
+import React, { useState } from "react";
+
 function Footer() {
+  const [mostrarAutores, setMostrarAutores] = useState(false);
+
+  const handleClick = () => {
+    setMostrarAutores(!mostrarAutores);
+  };
+
+
   return (
-    <footer>
-      <p> 2025 Festival Vilar de Mouros. Todos os direitos reservados.</p>
+    <footer onClick={handleClick}>
+      {mostrarAutores ? (
+        <p>Autores: Samuel Caldeira, Pedro Sena</p>
+      ) : (
+        <p>2025 Festival Vilar de Mouros. Todos os direitos reservados.</p>
+      )}
     </footer>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom/client'; // Importa createRoot do React 18
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import FormVoluntario from './components/FormVoluntario';
+import './index.css';
 
+// Cria a raiz da aplicação usando createRoot
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Renderiza a aplicação
+root.render(
+        <BrowserRouter>
+            <Routes>
+
+                <Route path="/" element={<App />} />
+                <Route path="/form-voluntario" element={<FormVoluntario />} />
+            </Routes>
+        </BrowserRouter>
+);
